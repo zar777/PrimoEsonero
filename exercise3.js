008af05948b44a54b810fe00a1ff28449650c603
Gianluca <gianluca.parente7@gmail.com>
2013-04-06 12:53:34 +0200

    finestre senza vetro

--- exercise3.js
+++ exercise3.js
@@ -2,3 +2,8 @@ var east=R([1,2])(PI/2)(SIMPLEX_GRID([[-p1.x,p5.x-p1.x],[-h,530-h]]));
 var north=R([0,2])(-PI/2)(SIMPLEX_GRID([[-h,530-h],[-p1.y,q7.y-p1.y]]));
 var south=T([0])([p5.x])(R([0,2])(-PI/2)(SIMPLEX_GRID([[-h,530-h],[-p1.y,q7.y-p1.y]])));
 var west=T([1])([q7.y])(R([1,2])(PI/2)(SIMPLEX_GRID([[-p1.x,p5.x-p1.x],[-h,530-h]])));
+
+
+var struct = STRUCT([east,north,south,west]);
+
+DRAW(struct);
--- exercise4.js
+++ exercise4.js
@@ -1,10 +1,14 @@
-function arc(alpha,r,R){
-var domain = DOMAIN([[0,alpha],[r,R]])([36,50]);
-var mapping=function(v){
-var a=v[0];
-var r=v[1];
-return[r*COS(a),r*SIN(a)]}
-var model=MAP(mapping)(domain);
-return model;
-}
-DRAW(arc(PI,53.5,66.5));
+var z1 = [390,520];
+var z2 = [450,520];
+var z3 = [390,470];
+var z4 = [450,470];
+
+var finestra2d = POLYLINE ([z1,z2,z4,z3,z1]);
+var finestra3d = EXTRUDE([9])(finestra2d);
+
+
+
+var fine3 = STRUCT(REPLICA(3)([finestra3d,T([1])([120])]));
+
+
+DRAW(fine3);
